f137a464bfe0e5d422b881bf07d869182eb6ec68
Jerson Rodas <jrodas19@gmail.com>
2022-01-19 16:07:48 -0500

    actualizado al 19-01-2021

---
+++ filtrando_dat.py
@@ -0,0 +1,93 @@
+from logging.handlers import DatagramHandler
+from unittest import addModuleCleanup
+
+
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+def run():
+    all_python_devs = [item['name'] for item in DATA if item['language']== 'python']
+    all_platzi_worker = [item['name'] for item in DATA if item['organization']== 'Platzi']
+    
+    adults = list(filter(lambda item: item['age'] >18, DATA))
+    adults = list(map(lambda item: item['name'], adults))
+    old_people = list(map(lambda item : item | {'old': item['age'] > 70}, DATA))
+    
+    print(all_python_devs)
+    print(all_platzi_worker)
+    print(adults)
+    print(old_people)
+    
+    
+if __name__ == '__main__':
+    run()
